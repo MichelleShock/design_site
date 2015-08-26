c489f8bacb91cecab8414cf6267f81471e668af0
Michelle Giralico <michelle@giralico.com>
2015-08-25 22:45:39 -0400

    added structure for photo gallery

--- javascript/mg-site.js
+++ javascript/mg-site.js
@@ -1,4 +1,63 @@
-var screen = window.screen.width
+$(document).ready(function() {
+	//UNSLIDER - IMAGE GALLERY
 
+
+
+
+}); //end ready
+
+
+
+
+
+/*
+var screenSize = window.screen.width
+var fullNav = "large"
+var mobNav = "small"
+
+//determine screen sizes to scale sit appropriately
 if (screen < 500 px){
+	return "small";
 }
+
+if (screen > 500 px){
+	return "large";
+}
+*/
+
+
+
+
+/*
+FOR NAVIGATION INTERACTION
+When Button is selected
+	hide other sections
+	show selected section
+	hide main image
+*/
+
+
+/*
+FOR SMALLER SCREENS
+When the screen is smaller than 500 px 
+	hide navigation bar
+	show dropdown navigation =
+	center head and subhead
+	shrink and center image 
+*/
+
+/*
+CONTACT FORM
+Form that emails me when filled out
+	- Name
+		checks to see if empty
+	- email
+		checks for valid email
+	- phone
+		checks for valid phone number 7 or 10 digit
+	- comments
+		checks for comments
+	- submit button
+		if empty or invalid returns error
+
+*/
