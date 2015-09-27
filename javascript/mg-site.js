b8054bcf86a34a4c6a2b619c02b75eb0aa160255
Michelle Giralico <michelle@giralico.com>
2015-09-27 14:38:56 -0400

    add portfolio

--- javascript/mg-site.js
+++ javascript/mg-site.js
@@ -1,3 +1,11 @@
+/*
+//Photo Gallery - modified version of https://github.com/michaelsoriano/bootstrap-photo-gallery/blob/master/photo-gallery.js
+
+
+*/
+
+
+
 $(document).ready(function() {
 	console.log('mgsite js is working');
 
@@ -33,7 +41,28 @@ $(document).ready(function() {
 	});
 
 
+//PORTFOLIO
 
+//MODAL
+		$('li img').on('click',function(){
+			var src = $(this).attr('src');
+			var img = '<img src="' + src + '" class="img-responsive"/>';
+			var index = $(this).parent('li').index();
+			var html = '';
+                html += img;
+                html += '<div style="height:25px;clear:both;display:block;">';
+                html += '<a class="controls next" href="'+ (index+2) + '">next &raquo;</a>';
+                html += '<a class="controls previous" href="' + (index) + '">wprev</a>';
+                html += '</div>';
+
+			$('#myModal').modal();
+			$('#myModal').on('shown.bs.modal', function(){
+				$('#myModal .modal-body').html(img);
+			});
+			$('#myModal').on('hidden.bs.modal', function(){
+            	$('#myModal .modal-body').html('');
+			});
+		});
 
 
 
@@ -61,7 +90,38 @@ $(document).ready(function() {
 
 }); //end ready
 
+$(document).on('click', 'a.controls', function(){
+	var index = $(this).attr('href');
+	var src = $('ul.row li:nth-child('+ index +') img').attr('src');
+	$('.modal-body img').attr('src', src);
 
+	var newPrevIndex = parseInt(index) - 1; 
+	var newNextIndex = parseInt(newPrevIndex) + 2; 
+	
+	if($(this).hasClass('previous')){               
+		$(this).attr('href', newPrevIndex); 
+		$('a.next').attr('href', newNextIndex);
+	}else{
+		$(this).attr('href', newNextIndex); 
+		$('a.previous').attr('href', newPrevIndex);
+	}
+	
+	var total = $('ul.row li').length + 1; 
+	//hide next button
+	if(total === newNextIndex){
+		$('a.next').hide();
+	}else{
+		$('a.next').show()
+	}            
+	//hide previous button
+	if(newPrevIndex === 0){
+		$('a.previous').hide();
+	}else{
+		$('a.previous').show()
+	}
+
+	return false;
+});
 
 
 /*
